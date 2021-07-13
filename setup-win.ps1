7984c22fbde7b6f93fcf15b5dbb3443b10e18b90
saitoh shigel <saitoh.shigel@gmail.com>
2021-07-13 16:42:19 +0900

    gitをscoopでinstallし、ssh鍵を生成する

--- setup-win.ps1
+++ setup-win.ps1
@@ -1,6 +1,9 @@
 # ユーザーアカウントでPowerShellを有効にし、ダイアログが出ないようにセキュリティを緩める
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Force -Scope CurrentUser
 
+# Write-VerboseをOnにして詳細メッセージが表示されるようにする
+$VerbosePreference = 'Continue'
+
 # enabled TLS1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bOR [Net.SecurityProtocolType]::Tls12
 
@@ -29,12 +32,18 @@ scoop bucket add extras
 # scoop install openssh
 
 # ssh鍵作成
+# Write-Verbose: 鍵を作成します。上書きする場合は(y)、しない場合は(n)を入力してEnterを押してください。
+Write-Verbose: making ssh key. Overwrite(y), Not Overwrite(n) and input Enter key.
 mkdir ${HOME}
-ssh-keygen -f ${HOME}\.ssh\id_rsa -t rsa -N '""'
+ssh-keygen -f ${HOME}\.ssh\id_rsa -t rsa -N '""' -q
 
 # ユーザーアカウントでPowerShellを無効にし、セキュリティ設定を安全なものに設定する
 Set-ExecutionPolicy -ExecutionPolicy Restricted -Force -Scope CurrentUser
 
-Read-Host "初期設定が完了しました。このあと公開鍵の内容が表示されます。\nGithubなどのDeploy Keyに設定してください。"
+# # Write-VerboseをOffにして詳細メッセージが非表示にする
+# $VerbosePreference = 'SilentlyContinue'
+
+#Read-Host "初期設定が完了しました。このあと公開鍵の内容が表示されます。\nGithubなどのDeploy Keyに設定してください。"
+Read-Host "Finished init configuration. showing public key\n Set Deploy Key. ex) Github"
 
 cat ${HOME}\.ssh\id_rsa.pub
