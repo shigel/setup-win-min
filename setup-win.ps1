302cfeef429d30df15f82edb1c261fdb377f2541
saitoh shigel <saitoh.shigel@gmail.com>
2021-07-13 16:12:42 +0900

    gitをscoopに変更。sshkey作成も自動化

--- setup-win.ps1
+++ setup-win.ps1
@@ -8,9 +8,6 @@ Install-PackageProvider -Force NuGet
 Install-PackageProvider -Force Chocolatey
 Install-PackageProvider -Force ChocolateyGet
 
-Install-Package -Force git -ProviderName ChocolateyGet
-Install-Package -Force openssh -ProviderName ChocolateyGet
-
 Install-Package -Force GoogleChrome -ProviderName ChocolateyGet
 
 cd ~
@@ -21,10 +18,23 @@ iwr -useb get.scoop.sh | iex
 # Scoop can utilize aria2 to use multi-connection downloads. Simply install aria2 through Scoop and it will be used for all downloads afterward.
 scoop install aria2
 
+# need git for adding bucket
+scoop install git
+
 # add bucket
 scoop bucket add extras
+
+# add apps
+# Win 10 Pro is installed openssh, but Win Server 2016 is not installed openssh
+# scoop install openssh
+
+# ssh鍵作成
+mkdir ${HOME}
+ssh-keygen -f ${HOME}\.ssh\id_rsa -t rsa -N '""'
 
 # ユーザーアカウントでPowerShellを無効にし、セキュリティ設定を安全なものに設定する
 Set-ExecutionPolicy -ExecutionPolicy Restricted -Force -Scope CurrentUser
 
-Read-Host "Install is finished. Enterを押してね！"
+Read-Host "初期設定が完了しました。このあと公開鍵の内容が表示されます。\nGithubなどのDeploy Keyに設定してください。"
+
+cat ${HOME}\.ssh\id_rsa.pub
